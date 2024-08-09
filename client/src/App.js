378e44ea91bd7868ccfa32c92ec4f12e9e1b8f9d
Harsh Bodgal <harshbodgal22@gmail.com>
2024-08-09 13:11:13 -0700

    added css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PostCreate from './PostCreate';
 import PostList from './PostList';
-
+import './App.css'
 export default function App() {
     return (
         <div>
-        <h1>Create Post</h1>
+        <h1 className="app-title">POST A THOUGHT</h1>
         <PostCreate />
         <PostList />
     </div>
